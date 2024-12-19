4a72fbde2b09dc14c22ecd3a9306f55f47bbf232
ReemNumam <s12323057@stu.najah.edu>
2024-12-19 18:48:25 +0200

    sec 1

--- src/components/clinics components/Section1.js
+++ src/components/clinics components/Section1.js
@@ -5,11 +5,13 @@ import Section1 from "./Section1";
 
 const Section1 = (props) => {
     return ( 
-      <section className="para&title">
-      <h1 className="title"> {props.title}</h1>
-      <p className="para">{props.para}</p>
-      </section>
-     <Clinic3/> 
+      <>
+        <section className="para&title">
+        <h1 className="title"> {props.title}</h1>
+        <p className="para">{props.para}</p>
+        </section>
+        <Clinic3/> 
+      </>
   );
 };
 
