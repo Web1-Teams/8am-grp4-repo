afcf469dec77491f82c163a8a53d9fa86e587a63
prog-momen <s12325799@stu.najah.edu>
2024-12-20 14:56:46 +0200

    تعديل بسيط على كومبوننت السكشن

--- src/components/clinics components/Section.js
+++ src/components/clinics components/Section.js
@@ -1,25 +1,17 @@
-import Clinic3 from "./Clinic3";
-import Section1 from "./Section1";
 
 
-
-const Section1 = (props) => {
+const Section = (props) => {
     return ( 
       <>
         <section className="para&title">
         <h1 className="title"> {props.title}</h1>
         <p className="para">{props.para}</p>
         </section>
-        <Clinic3/> 
       </>
   );
 };
 
 
 
-
-
-
-  
-  export default Section1;
+  export default Section;
   
