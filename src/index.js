9a699693dd890a6c502d7fafd3aea231b7ceb7d9
manarimad <eng.manar.imad@gmail.com>
2024-12-22 20:02:06 +0200

    add Nutrition Value table React component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Clinic3 from './components/clinics components/Clinic3';
+import NutritionTable from './components/NVTable/NutritionTable';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<>
-<Clinic3/>
-</>
+
+<NutritionTable/>
+
 );
