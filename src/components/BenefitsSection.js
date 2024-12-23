ac431ba56660700feb3f370cb8d1347d62157942
fawzirahhal <fawzirahhalll@gmail.com>
2024-12-23 04:17:22 -0800

    تم اضافة ملفات Benefits مع السكشن تبعها

---
+++ src/components/BenefitsSection.js
@@ -0,0 +1,41 @@
+import Benefits from "./Benefits"
+
+
+
+
+
+
+const BenefitsSection=(props)=>{
+return(
+<>
+<Benefits
+tital="Benefits of Meditation and Mindfulness ((مش رح اتركهم كلهم&ممكن احطهم اول اشي
+Mental Benefits"
+b1="Reduces stress and anxiety."
+b2="Enhances focus and concentration."
+b3="Boosts creativity and problem-solving skills."
+/>
+<Benefits
+tital="Benefits of Meditation and Mindfulness ((مش رح اتركهم كلهم&ممكن احطهم اول اشي
+Mental Benefits"
+b1="Reduces stress and anxiety."
+b2="Enhances focus and concentration."
+b3="Boosts creativity and problem-solving skills."
+/>
+<Benefits
+tital="Benefits of Meditation and Mindfulness ((مش رح اتركهم كلهم&ممكن احطهم اول اشي
+Mental Benefits"
+b1="Reduces stress and anxiety."
+b2="Enhances focus and concentration."
+b3="Boosts creativity and problem-solving skills."
+/>
+</>
+
+)
+
+
+
+
+}
+
+export default BenefitsSection
