8fdee81eeb6a2583ac017c6771855ef399ed0164
manarimad <eng.manar.imad@gmail.com>
2024-12-26 17:32:50 +0200

    add html files for clinic 2 & fix some errors on BookList file

--- src/components/Books/BookList.js
+++ src/components/Books/BookList.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./BookList.css";
 import book1 from "../../assests/Images/images for c2/book1.jpg";
+import book2 from "../../assests/Images/images for c2/book2.png";
+import book3 from "../../assests/Images/images for c2/book3.jpg";
+import book4 from "../../assests/Images/images for c2/book4.jpg";
 import Book from "./Book";
 
 function BookList() {
@@ -16,17 +19,24 @@ function BookList() {
             image={book1}
           />
           <Book
-            title="Skin Care Tips: Complete Guide to Taking Care of Your Skin Naturally"
+            title="كيمياء البشرة"
             description="يتحدث هذا الكتاب عن ..."
-            link="https://www.barnesandnoble.com/w/skin-care-tips-annie-ramsey/1122174683"
-            image={book1}
+            link="https://www.neelwafurat.com/itempage.aspx?id=lbb255840-238679&search=books"
+            image={book2}
           />
           <Book
-            title="Skin Care Tips: Complete Guide to Taking Care of Your Skin Naturally"
+            title="أسرار العناية بالبشرة (نظرية الجمال المتكاملة)"
             description="يتحدث هذا الكتاب عن ..."
-            link="https://www.barnesandnoble.com/w/skin-care-tips-annie-ramsey/1122174683"
-            image={book1}
+            link="https://www.neelwafurat.com/itempage.aspx?id=lbb387719-384972&search=books"
+            image={book3}
+
           />
+          <Book
+            title="طرق سهلة ومجربة للعناية بالشعر"
+            description="يتحدث هذا الكتاب عن ..."
+            link="https://www.neelwafurat.com/itempage.aspx?id=lbb197488-170288&search=books"
+            image={book4}
+         />
 
         </div>
       </div>
