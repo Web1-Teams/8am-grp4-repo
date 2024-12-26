71481e5e0398f31c5e48276d845f883f59c6c44a
prog-momen <s12325799@stu.najah.edu>
2024-12-26 17:50:12 +0200

    شوية تعديلات

--- src/components/Main page components/Main.js
+++ src/components/Main page components/Main.js
@@ -3,15 +3,13 @@ import './styles_main ar.css'
 import TopTopic from "../TopTopic"
 import Footer from "../Footer"
 import Review from "../Review"
-import DoctorSection1 from "../DoctorSection1"
+
 const Main=()=>{
     return (
         <>
         <NavBar/>
         <TopTopic topic1="Welcome" clinic1="Main"/>
-        <br/>
-        <DoctorSection1/>
-        <br/>
+
         <Review/>
         <Footer/>
 </>
--- src/components/TopTopic.js
+++ src/components/TopTopic.js
@@ -5,13 +5,13 @@ import '../styles/toptopic.css'
 const TopTopic=(props)=>{
     return(
 <div>
-<h2>{props.clinic1}</h2>
+<h2>Hot Topic</h2>
 <div className="card1">
     <p><span>{props.topic1}</span></p>
-    <p><span>HOVER ME</span></p>
-    <p><span>HOVER ME</span></p>
-    <p><span>HOVER ME</span></p>
-    <p><span>HOVER ME</span></p>
+    <p><span>{props.topic2}</span></p>
+    <p><span>{props.topic3}</span></p>
+    <p><span>{props.topic4}</span></p>
+    <p><span>{props.topic5}</span></p>
 
 
 </div>
