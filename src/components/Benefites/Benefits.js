39262f5b5e7d55d2312c8d71977ff440b46471de
sedra-fares <s12325061@stu.najah.edu>
2025-01-03 22:30:29 +0200

    clinic3 styling

--- src/components/Benefites/Benefits.js
+++ src/components/Benefites/Benefits.js
@@ -1,14 +1,13 @@
-import './benefits.css'
 
 
 const Benefits = (props) => {
     return (
-        <div>
-            <h3 className="header">{props.title}</h3>
+        <div className="benefits-card">
+            <h3 className="benefits-title">{props.title}</h3>
             <ul>
-                <li className="benefits">{props.b1}</li>
-                <li className="benefits">{props.b2}</li>
-                <li className="benefits">{props.b3}</li>
+                <li className="benefits-item">{props.b1}</li>
+                <li className="benefits-item">{props.b2}</li>
+                <li className="benefits-item">{props.b3}</li>
             </ul>
         </div>
     )
