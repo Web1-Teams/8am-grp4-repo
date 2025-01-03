82ae0a0aa012394e88d3242a1d70dd350459820c
ReemNumam <s12323057@stu.najah.edu>
2025-01-03 14:55:25 +0200

    reem

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import NavBar from "./components/navbar/Navbar";
 import Main from './components/Main page components/Main';
 // import Clinic1 from './components/clinics-components/clinic1/Clinic1';
 import TopTopic from "./components/toptopic/TopTopic";
-import Review from "./components/review/Review";
-import Footer from "./components/footer/Footer";
+import Review from "./components/Review/Review";
+import Footer from "./components/Footer/Footer";
 import Clinic3 from './components/clinics-components/Clinic3/Clinic3';
 import Canvas from './components/clinic3ComponentsTemp/canvass/Canvas';
 
