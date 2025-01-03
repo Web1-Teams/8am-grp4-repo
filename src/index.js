5a613848630820d3eff5f2b141145181d1469f43
prog-momen <s12325799@stu.najah.edu>
2025-01-03 20:37:34 +0200

    routing start

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
-import Clinic3 from './components/clinics components/Clinic3/Clinic3';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Main from "./components/Main page components/Main";
+import Clinic1 from './components/clinics-components/clinic1/Clinic1'
 
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<StrictMode>
-    <App/>
-</StrictMode>
-
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar/>}>
+          <Route path="/" index element={<Main/>}/>
+          <Route path="/Page 1" element={<Clinic1/>}/>
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    <App />
+  </React.StrictMode>
 );
