8f4ec7a050eb7c153a45a6cbdbd6b96b6d4446b0
sedra-fares <s12325061@stu.najah.edu>
2025-01-04 22:00:34 +0200

    newwwwwwwwwww

--- src/components/Main page components/Main.js
+++ src/components/Main page components/Main.js
@@ -1,10 +1,12 @@
 import './styles_main ar.css'
 import Review from "../Review/Review"
+import Main_about from '../main_about/Main_about';
 
 
 const Main=()=>{
     return (
         <>
+        <Main_about/>
         <Review/>
 </>
     )
