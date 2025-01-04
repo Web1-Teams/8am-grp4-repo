9392a60312e04d83dc43a4d3f0c3e84ffbfd36dc
manarimad <eng.manar.imad@gmail.com>
2025-01-04 22:27:04 +0200

    format document for some files

--- src/components/clinic2 Components/IntroSection.js
+++ src/components/clinic2 Components/IntroSection.js
@@ -1,20 +1,23 @@
 import React from "react";
-import "../../components/clinics-components/clinic2/Styles_C2.css"
+import "../../components/clinics-components/clinic2/Styles_C2.css";
 
-function IntroSection (props) {
+function IntroSection(props) {
   return (
     <div className="intro-section">
       <h1 className="intro-title">{props.title}</h1>
-      <p className="intro-subtitle">{props.subtitle1} <br/>
-      {props.subtitle2} <span className="intro-highlight">{props.specific1}</span> {props.specific3}
+      <p className="intro-subtitle">
+        {props.subtitle1} <br />
+        {props.subtitle2}{" "}
+        <span className="intro-highlight">{props.specific1}</span>{" "}
+        {props.specific3}
       </p>
       <p className="intro-subtitle">
-      {props.subtitle4} <span className="intro-highlight">{props.specific2}</span> {props.subtitle5}
-  </p>
-  <p className="intro-subtitle">
-   {props.subtitle6}
-  </p>
+        {props.subtitle4}{" "}
+        <span className="intro-highlight">{props.specific2}</span>{" "}
+        {props.subtitle5}
+      </p>
+      <p className="intro-subtitle">{props.subtitle6}</p>
     </div>
-  )
-};
+  );
+}
 export default IntroSection;
