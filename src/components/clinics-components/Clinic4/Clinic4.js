e7babeb6f567b56cc083b8ea6d8881c399f50383
mahmoud-saleh777 <s12326423@stu.najah.edu>
2025-01-04 23:14:08 +0200

    update

--- src/components/clinics-components/Clinic4/Clinic4.js
+++ src/components/clinics-components/Clinic4/Clinic4.js
@@ -11,8 +11,8 @@ const Clinic4=()=>{
 
        <div> 
            <Header
-            ClinicName="Mental Health Clinic"
-            ClinicAyah="{Those who have believed and whose hearts are assured by the remembrance of Allah. Unquestionably, by the remembrance of Allah hearts are assured.}"
+            ClinicName="عيادة التغذيه "
+            ClinicAyah="{ يَا أَيُّهَا النَّاسُ كُلُوا مِمَّا فِي الْأَرْضِ حَلَالًا طَيِّبًا وَلَا تَتَّبِعُوا خُطُوَاتِ الشَّيْطَانِ إِنَّهُ لَكُمْ عَدُوٌّ مُبِينٌ.}"
             className="page4Background"
             />
          </div> 
